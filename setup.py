3b72ee4b7cccf35f5c4ef37c90c99dc24add0f7f
Swathi Vemuru <swathi.vemuru.06@gmail.com>
2026-03-05 17:25:54 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simple_chatbot",
+    version="0.1.0",
+    author="Swathi Vemuru",
+    author_email="swathi.vemuru.06@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
