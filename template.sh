5d13635f74bc68f860d7eb55b4942502c9d1585b
Swathi Vemuru <swathi.vemuru.06@gmail.com>
2026-04-05 10:25:43 +0530

    minor changes

--- template.sh
+++ template.sh
@@ -2,8 +2,9 @@
 
 mkdir -p src
 mkdir -p research
+mkdir -p data
 
-touch src/__init__.py
+
 touch src/helper.py
 touch src/prompt.py
 
@@ -13,5 +14,7 @@ touch app.py
 
 touch research/trials.ipynb
 touch requirements.txt
+touch README.md
+touch source_index.py
 
 echo "Directory structure created successfully!"
